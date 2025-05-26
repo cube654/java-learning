781a369441a019140fc60c32051506cd0387ddf9
cube654 <3550355312@qq.com>
2025-05-26 20:29:21 +0800

    Add files via upload

---
+++ code/day4/LotteryDrawing.java
@@ -0,0 +1,50 @@
+/*
+今天什么代码都没有写，看了三篇代码，感觉这是最重要的代码
+*/
+
+import java.util.*;
+
+/**
+ * This program demonstrates array manipulation.
+ * @version 1.20 2004-02-10
+ * @author Cay Horstmann
+ */
+public class LotteryDrawing
+{
+   public static void main(String[] args)
+   {
+      Scanner in = new Scanner(System.in);
+
+      System.out.print("How many numbers do you need to draw? ");
+      int k = in.nextInt();
+
+      System.out.print("What is the highest number you can draw? ");
+      int n = in.nextInt();
+
+      // fill an array with numbers 1 2 3 . . . n
+      int[] numbers = new int[n];
+      for (int i = 0; i < numbers.length; i++)
+         numbers[i] = i + 1;
+
+      // draw k numbers and put them into a second array
+      int[] result = new int[k];
+      for (int i = 0; i < result.length; i++)
+      {
+         // make a random index between 0 and n - 1
+         int r = (int) (Math.random() * n);//这个是随机数的显示方法，重要
+
+         // pick the element at the random location
+         result[i] = numbers[r];
+
+         // move the last element into the random location
+         numbers[r] = numbers[n - 1];
+         n--;
+      }
+
+      // print the sorted array
+      Arrays.sort(result);
+      System.out.println("Bet the following combination. It'll make you rich!");
+      for (int r : result)
+         System.out.println(r);
+   }
+}
